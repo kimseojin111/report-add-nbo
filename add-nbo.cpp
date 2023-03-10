4b29137064b3fd5d77e6b18c1e39eeb89c28dc09
seojin <palmtreejin@gmail.com>
2023-03-10 17:06:15 +0900

    clean code

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -2,49 +2,8 @@
 #include <stdint.h> // for uint8_t
 #include <stdio.h> // for printf
 
-void dump(void* p, size_t n) {
-	uint8_t* u8 = static_cast<uint8_t*>(p);
-	size_t i = 0;
-	while (true) {
-		printf("%02X ", *u8++);
-		if (++i >= n) break;
-		if (i % 8 == 0)
-			printf("\n");
-	}
-	printf("\n");
-}
-
-void write_4660() {
-	uint16_t port = 4660; // 0x1234
-	printf("port number = %d\n", port);
-	dump(&port, sizeof(port));
-}
-
-
-uint16_t ntohs(uint16_t x){
-	return (x>>8)|(x<<8);
-}
-
-
 uint32_t ntohl(uint32_t x){
 	return (x<<24)|(x<<8)&(0x00ff0000)|(x>>8)&(0x0000ff00)|(x>>24);
-}
-
-
-
-void  write_0x1234() {
-	uint8_t network_buffer[] = { 0x12, 0x34 };
-	uint16_t* p = reinterpret_cast<uint16_t*>(network_buffer);
-	uint16_t n = ntohs(*p); // TODO
-	printf("16 bit number=0x%x\n", n);
-}
-
-void  write_0x12345678() {
-	uint8_t network_buffer[] = { 0x12, 0x34, 0x56, 0x78 };
-	uint32_t* p = reinterpret_cast<uint32_t*>(network_buffer);
-	uint32_t n = ntohl(*p); // TODO
-	//uint32_t n = *p;
-	printf("32 bit number=0x%x\n", n);
 }
 
 int main(int argc, char* argv[]) {
